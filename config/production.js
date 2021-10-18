befcb5b614ea28e793dd32acf43e7fcefe89cf35
lewism <lewis@sendy.co.ke>
2021-10-18 15:28:06 +0300

    Revet fulfilment urls

--- config/production.js
+++ config/production.js
@@ -4,8 +4,8 @@ module.exports = {
   AUTH: 'https://auth.sendyit.com/',
   GOOGLE_API_KEY: 'AIzaSyBr3G1ZoEidlWAw_NOO11NZsVidPuF6zeo',
   GOOGLE_API_KEY_TEST: 'AIzaSyC4UV3vk4qedVcCXDdpY7ZBNb4IQAM4vY8',
-  FIREBASE_CONFIG: "{apiKey: 'AIzaSyCgetfUGhkVy1c_QePUgxtyr8vDOvB0Ru4',authDomain: 'buyer-fulfillment.firebaseapp.com',projectId: 'buyer-fulfillment',storageBucket: 'buyer-fulfillment.appspot.com',messagingSenderId: '809840261046',appId: '1:809840261046:web:173c5b76f2f807c1657317',measurementId: 'G-7CLVXLT1BW'}",
+  FIREBASE_CONFIG: "{apiKey: 'AIzaSyDAAvZPAgy7HX8JUqxWsFxn28ixGoOnHPs', authDomain: 'sendy-fulfilment.firebaseapp.com', projectId: 'sendy-fulfilment', storageBucket: 'sendy-fulfilment.appspot.com', messagingSenderId: '724697801657', appId: '1:724697801657:web:69355a1ba4a87949430c68', measurementId: 'G-YFWVL6YKF7'}",
   VAPIDKEY: 'BGNlgSsJAfNM7suOwAm0LyVX7H5GCB9lUosFfOQy99Qrhr7fpPGE2xd5h7qekXL0WYwIf2K26CCMz3wWMiG49jY',
   SPOTLIGHT_SERVER: 'https://stoplight.io/mocks/sendy/fulfilment-service/20985211/',
-  FULFILMENT_SERVER: 'https://auth.sendyit.com/fulfilment/v1/',
+  FULFILMENT_SERVER: 'https://fulfillment-biz-logic-service-dev.sendyit.com/v1/',
 };
