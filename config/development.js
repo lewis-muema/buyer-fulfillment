ac1f862ac68f01b6c05679a71d96c560d932bfd8
Dorcas Cherono <dorcas@sendyit.com>
2022-09-16 12:14:42 +0300

    google optimize on buyer-link fix

--- config/development.js
+++ config/development.js
@@ -4,7 +4,6 @@ module.exports = {
   AUTH: 'https://authtest.sendyit.com/',
   GOOGLE_API_KEY: 'AIzaSyBowCKmIDSOLkit2Qn6ZDaLdwnCvPoBF9I',
   GOOGLE_API_KEY_TEST: 'AIzaSyC4UV3vk4qedVcCXDdpY7ZBNb4IQAM4vY8',
-  FIREBASE_CONFIG: "{ apiKey: 'AIzaSyCgetfUGhkVy1c_QePUgxtyr8vDOvB0Ru4',authDomain: 'buyer-fulfillment.firebaseapp.com',projectId: 'buyer-fulfillment',storageBucket: 'buyer-fulfillment.appspot.com',messagingSenderId: '809840261046',appId: '1:809840261046:web:173c5b76f2f807c1657317',measurementId: 'G-7CLVXLT1BW'}",
   VAPIDKEY: 'BGNlgSsJAfNM7suOwAm0LyVX7H5GCB9lUosFfOQy99Qrhr7fpPGE2xd5h7qekXL0WYwIf2K26CCMz3wWMiG49jY',
   SPOTLIGHT_SERVER: 'https://stoplight.io/mocks/sendy/fulfilment-service/20985211/',
   FULFILMENT_SERVER: 'https://authtest.sendyit.com/fulfilment/v1/',
